ee2bcf7610d6c3f9231bd8d8591eef75463ae8b2
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-14 10:24:53 +0200

    Update coq.lua
    * Coq setup only if python is present

--- lua/plugins/coq.lua
+++ lua/plugins/coq.lua
@@ -1,19 +1,21 @@
 --   vi: tw=100
 -- lua/plugins/coq.lua @ Leonardo Valeri Manera 2022
 
-require("coq") {}
+if vim.fn.has('python3') then
+    require("coq") {}
 
-require("coq_3p") {
-  { src = "builtin/ada"     },
-  { src = "builtin/c"       },
-  { src = "builtin/clojure" },
-  { src = "builtin/css"     },
-  { src = "builtin/haskell" },
-  { src = "builtin/html"    },
-  { src = "builtin/js"      },
-  { src = "builtin/php"     },
-  { src = "builtin/syntax"  },
-  { src = "builtin/xml"     },
-  { src = "nvimlua", short_name = "nLUA" },
-}
+    require("coq_3p") {
+        { src = "builtin/ada"     },
+        { src = "builtin/c"       },
+        { src = "builtin/clojure" },
+        { src = "builtin/css"     },
+        { src = "builtin/haskell" },
+        { src = "builtin/html"    },
+        { src = "builtin/js"      },
+        { src = "builtin/php"     },
+        { src = "builtin/syntax"  },
+        { src = "builtin/xml"     },
+        { src = "nvimlua", short_name = "nLUA" },
+    }
+end
 
