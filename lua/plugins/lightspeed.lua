f7bd923022718c4a74e949c95c7a7d1aa41df1ec
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-16 02:23:28 +0200

    Update lightspeed.lua
    * consistency

--- lua/plugins/lightspeed.lua
+++ lua/plugins/lightspeed.lua
@@ -1,7 +1,7 @@
 --   vi: tw=100
 -- lua/plugins/lightspeed.lua @ Leonardo Valeri Manera 2022
 
-require'lightspeed'.setup {
+require('lightspeed').setup {
     match_only_the_start_of_same_char_seqs = true,
 }
 
