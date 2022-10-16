3e70472b1b21446055a0482a661c3ee5c3821dc9
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-16 23:52:43 +0200

    Legendary
    * Setup keymaps
    * Setup which-key prefixes

--- lua/plugins/legendary.lua
+++ lua/plugins/legendary.lua
@@ -1,5 +1,34 @@
 --   vi: tw=100
 -- lua/plugins/legendary.lua @ Leonardo Valeri Manera 2022
 
-require('legendary').setup()
+local legendary = require("legendary")
+
+legendary.setup({ auto_register_which_key = true })
+
+local filters = require("legendary.filters")
+
+vim.keymap.set({'n', 'v', 'o'}, '<leader>ha',
+    function() legendary.find({ kind = 'autocmds' }) end,
+    { desc = "Legendary autocommands" })
+vim.keymap.set({'n', 'v', 'o'}, '<leader>hf',
+    function() require("legendary").find({ kind = 'functions' }) end,
+    { desc = "Legendary functions" })
+vim.keymap.set({'n', 'v', 'o'}, '<leader>hc',
+    function() require("legendary").find({ kind = 'commands' }) end,
+    { desc = "Legendary commands" })
+vim.keymap.set({'n', 'v', 'o'}, '<leader>hkm',
+    function() require("legendary").find({ kind = 'keymaps', filters  = filters.current_mode() }) end,
+    { desc = "Legendary keymaps (current)" })
+vim.keymap.set({'n', 'v', 'o', 'i', 'c', 't'}, '<C-k>',
+    function() require("legendary").find({ kind = 'keymaps', filters  = filters.current_mode() }) end,
+    { desc = "Legendary keymaps (current)" })
+vim.keymap.set({'n', 'v', 'o'}, '<leader>hki',
+    function() require("legendary").find({ kind = 'keymaps', filters  = filters.mode('i') }) end,
+    { desc = "Legendary keymaps (insert)" })
+vim.keymap.set({'n', 'v', 'o'}, '<leader>hkc',
+    function() require("legendary").find({ kind = 'keymaps', filters  = filters.mode('c') }) end,
+    { desc = "Legendary keymaps (command)" })
+vim.keymap.set({'n', 'v', 'o'}, '<leader>hkt',
+    function() require("legendary").find({ kind = 'keymaps', filters  = filters.mode('t') }) end,
+    { desc = "Legendary keymaps (terminal)" })
 
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -8,6 +8,7 @@ require("which-key").register({
     c = { name = "Quickfix" },
     t = { name = "Tabs" },
     s = { name = "Syntax" },
+    h = { name = "Legendary" },
     [","] = "which_key_ignore",
     },
     { prefix = "<leader>" })
@@ -16,3 +17,9 @@ require("which-key").register({
     y = { name = "Syntax" },
     },
     { prefix = "<leader>s" })
+
+require("which-key").register({
+    k = { name = "Keymaps" },
+    },
+    { prefix = "<leader>h" })
+
