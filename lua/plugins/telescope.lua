e0e0fe734210e32124a1c16cee2359af6ebc885f
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-16 04:31:30 +0200

    Update telescope.lua
    * Map descriptions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -52,31 +52,35 @@ extension("yank_history")
 
 -- binds
 -- <leader>lp -> Show list of pickers
-map('n', "<leader>lp", builtin.pickers, {})
--- <leader>lg -> Search in current buffer
-map('n', "<leader>lg", builtin.current_buffer_fuzzy_find, {})
+map('n', "<leader>lp", builtin.pickers, { desc = "Telescope pickers" })
+-- <leader>lgb -> Grep in current buffer
+map('n', "<leader>lgb", builtin.current_buffer_fuzzy_find, { desc = "Telescope Grep in buffer" })
+-- <leader>lgd -> Grep in current directory
+map('n', "<leader>lgd", builtin.live_grep, { desc = "Telescope Grep in CWD" })
 -- <leader>lb -> Search buffers
-map('n', "<leader>lb", builtin.buffers, {})
--- <leader>lf -> File browser
-map('n', "<leader>lf", require("telescope").extensions.file_browser.file_browser, {})
--- <leader>lta -> Search for Tags in all buffers
-map('n', "<leader>lta", builtin.tags, {})
--- <leader>ltc -> Search for Tags in current buffer
-map('n', "<leader>ltc", builtin.current_buffer_tags, {})
--- <leader>lto -> Search for Tags in current buffer
-map('n', "<leader>lto", require("telescope").extensions.ctags_outline.outline, {})
--- <leader>lma -> Search for Marks in all buffers
-map('n', "<leader>lma", require("telescope").extensions.vim_bookmarks.all, {})
--- <leader>lmc -> Search for Marks in current buffer
-map('n', "<leader>lmc", require("telescope").extensions.vim_bookmarks.current_file, {})
+map('n', "<leader>lb", builtin.buffers, { desc = "Telescope buffers" })
+-- <leader>lff -> Find files
+map('n', "<leader>lff", builtin.find_files, { desc = "Telescope files in CWD" })
+-- <leader>lfb -> File browser
+map('n', "<leader>lfb", require("telescope").extensions.file_browser.file_browser, { desc = "Telescope file browser" })
+-- <leader>lta -> Search Tags in all buffers
+map('n', "<leader>lta", builtin.tags, { desc = "Telescope all Tags" })
+-- <leader>ltb -> Search Tags in current buffer
+map('n', "<leader>ltb", builtin.current_buffer_tags, { desc = "Telescope buffer Tags" })
+-- <leader>lto -> Search Tags in outline
+map('n', "<leader>lto", require("telescope").extensions.ctags_outline.outline, { desc = "Telescope outline Tags" })
+-- <leader>lma -> Search Marks in all buffers
+map('n', "<leader>lma", require("telescope").extensions.vim_bookmarks.all, { desc = "Telescope all Marks" })
+-- <leader>lmc -> Search Marks in current buffer
+map('n', "<leader>lmc", require("telescope").extensions.vim_bookmarks.current_file, { desc = "Telescope buffer Marks" })
 -- <leader>lhy -> Search the Yank Ring
-map('n', "<leader>lhy", ":Telescope yank_history", { silent = true })
+map('n', "<leader>lhy", ":Telescope yank_history", { silent = true, desc = "Telescope Yank history" })
 -- <leader>lhs -> Search search history
-map('n', "<leader>lhs", builtin.search_history, {})
+map('n', "<leader>lhs", builtin.search_history, { desc = "Telescope Search history" })
 -- <leader>lhc -> Search command history
-map('n', "<leader>lhc", builtin.command_history, {})
+map('n', "<leader>lhc", builtin.command_history, { desc = "Telescope Command history" })
 -- <leader>lr -> Search MRU
-map('n', "<leader>lr", require("telescope").extensions.recent_files.pick, {})
+map('n', "<leader>lr", require("telescope").extensions.recent_files.pick, { desc = "Telescope MRU" })
 -- <leader>ltt -> Search for tabs
-map('n', "<leader>ltt", require('telescope-tabs').list_tabs, {})
+map('n', "<leader>ltt", require('telescope-tabs').list_tabs, { desc = "Telescope Tabs" })
 
