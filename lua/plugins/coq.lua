2033c0740ae750be4ca5f8e720d018714dbbc478
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-16 02:34:46 +0200

    Update coq.lua
    * nvimlua only for plugins
    * main require after settings are set

--- lua/plugins/coq.lua
+++ lua/plugins/coq.lua
@@ -2,12 +2,12 @@
 -- lua/plugins/coq.lua @ Leonardo Valeri Manera 2022
 
 if vim.fn.has('python3') then
-    require("coq") {}
-
     vim.g.coq_settings = {
         ["auto_start"] = 'shut-up',
         ["keymap.manual_complete_insertion_only"] = true
     }
+
+    require("coq") {}
 
     require("coq_3p") {
         { src = "builtin/ada"     },
@@ -20,7 +20,7 @@ if vim.fn.has('python3') then
         { src = "builtin/php"     },
         { src = "builtin/syntax"  },
         { src = "builtin/xml"     },
-        { src = "nvimlua", short_name = "nLUA" },
+        { src = "nvimlua", short_name = "nLUA", conf_only = true },
     }
 end
 
