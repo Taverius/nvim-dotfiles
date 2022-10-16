ce4b279bfd7122796bbc23b34f4b754fa9a7e91a
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-16 02:23:39 +0200

    Update lualine.lua
    * Update extensions

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,5 @@
 --   vi: tw=100
 -- lua/plugins/lualine.lua @ Leonardo Valeri Manera 2022
 
-require('lualine').setup { extensions = { 'fugitive', 'nvim-tree' } }
+require('lualine').setup { extensions = { 'fugitive', 'fern', 'fzf', 'quickfix' } }
 
