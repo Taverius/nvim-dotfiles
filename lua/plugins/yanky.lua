56fca78cd78cb63ee32562bedd8690a9920e7f43
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-17 00:40:35 +0200

    Update yanky.lua
    * Map descriptions

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -5,12 +5,12 @@ require("yanky").setup({})
 
 -- Bindings
 -- Override the default p
-vim.keymap.set({"n","x"}, "p", "<Plug>(YankyPutAfter)")
-vim.keymap.set({"n","x"}, "P", "<Plug>(YankyPutBefore)")
+vim.keymap.set({"n","x"}, "p", "<Plug>(YankyPutAfter)", { desc = "Yanky Put After" })
+vim.keymap.set({"n","x"}, "P", "<Plug>(YankyPutBefore)", { desc = "Yanky Put Before" })
 -- Also replace the default gp with yoink paste so we can toggle paste in this case too
-vim.keymap.set({"n","x"}, "gp", "<Plug>(YankyGPutAfter)")
-vim.keymap.set({"n","x"}, "gP", "<Plug>(YankyGPutBefore)")
+vim.keymap.set({"n","x"}, "gp", "<Plug>(YankyGPutAfter)", { desc = "Yanky GPut After" })
+vim.keymap.set({"n","x"}, "gP", "<Plug>(YankyGPutBefore)", { desc = "Yanky GPut Before" })
 -- Swap paste back & forth in history
-vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")
-vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
+vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)", { desc = "Yanky Cycle forward" })
+vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)", { desc = "Yanky Cycle backward" })
 
