eba516c79dbf9600bad014c8e604e6576dfe8db5
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-16 08:52:27 +0200

    Undotree
    * Convert configuration to new undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,8 +1,15 @@
 --   vi: tw=100
 -- lua/plugins/undotree.lua @ Leonardo Valeri Manera 2022
 
--- set layout
-vim.g.undotree_WindowLayout = 2
--- <leader>lu -> Undo
-vim.keymap.set({'n', 'v', 'o'}, "<leader>lu", ":UndotreeToggle<CR>", {silent = true})
+local undotree = require('undotree')
+
+-- Options
+undotree.setup({
+  float_diff = true,  -- using float window previews diff, set this `true` will disable layout option
+  layout = "left_bottom", -- "left_bottom", "left_left_bottom"
+  -- ignore_filetype = { 'Undotree', 'UndotreeDiff', 'qf', 'TelescopePrompt', 'spectre_panel', 'tsplayground' },
+})
+
+-- <leader>u -> Undo
+vim.keymap.set('n', "<leader>u", undotree.toggle, {silent = true, desc = "Undotree toggle" })
 
