c146cab3d07e8a9ae490c97a9883aa4cd441a613
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-18 23:20:22 +0200

    Update lightspeed.lua
    * Which-key registrations

--- lua/plugins/lightspeed.lua
+++ lua/plugins/lightspeed.lua
@@ -5,3 +5,20 @@ require('lightspeed').setup {
     match_only_the_start_of_same_char_seqs = true,
 }
 
+-- Which-keu
+require("which-key").register({
+    s = { name = "Search forward", mode = 'nv' },
+    S = { name = "Search backward", mode = 'nv' },
+    z = { name = "Select until forward", mode = 'o' },
+    Z = { name = "Select until backward", mode = 'o' },
+    x = { name = "Select including forward", mode = 'o' },
+    X = { name = "Search including backward", mode = 'o' },
+    gs = { name = "Search in next windows", mode = 'n' },
+    gS = { name = "Search in previous windows", mode = 'n' },
+    f = { name = "Search forward including", mode = 'nvo' },
+    F = { name = "Search backward including", mode = 'nvo' },
+    t = { name = "Search forward until", mode = 'nvo' },
+    T = { name = "Search backward until", mode = 'nvo' },
+    })
+
+
