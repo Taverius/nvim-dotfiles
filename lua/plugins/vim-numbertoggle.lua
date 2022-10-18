be7bb6f44fe5c2902662cb97d63ad6c19525e947
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-18 23:31:14 +0200

    Update vim-numbertoggle.lua
    * Can do this in one single Lua map
    * Add map description

--- lua/plugins/vim-numbertoggle.lua
+++ lua/plugins/vim-numbertoggle.lua
@@ -2,6 +2,9 @@
 -- lua/plugins/vim-numbertoggle.lua @ Leonardo Valeri Manera 2022
 
 -- F3 -> Toggle relative number
-vim.keymap.set({'n', 'v', 'o'}, "<F3>", ":set relativenumber!<cr>", { silent = true })
-vim.keymap.set('i', "<F3>", "<C-o>:set relativenumber!<cr>", { silent = true })
+vim.keymap.set({'n', 'v', 'o', 'i'}, "<F3>",
+    function()
+        vim.wo.relativenumber = not vim.wo.relativenumber
+    end,
+    { desc = "Toggle relative numbers" })
 
