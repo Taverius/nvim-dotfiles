ade4ddf69151f1c67d67f0f9c0f206dd89a70a53
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-19 11:02:27 +0200

    Update is.lua
    * whoops

--- lua/plugins/is.lua
+++ lua/plugins/is.lua
@@ -14,7 +14,7 @@ vim.keymap.set({'n', 'v', 'o'}, "g#", "<Plug>(asterisk-g#)<Plug>(is-nohl-1)",
 vim.keymap.set({'n', 'v', 'o'}, "z*", "<Plug>(asterisk-z*)<Plug>(is-nohl-10)",
     { silent = true, remap = true, desc = "Search forward (in-place)" })
 vim.keymap.set({'n', 'v', 'o'}, "gz*", "<Plug>(asterisk-gz*)<Plug>(is-nohl-10)",
-    { silent = true, remap = true desc = "Partial Search forward (in-place)" })
+    { silent = true, remap = true, desc = "Partial Search forward (in-place)" })
 vim.keymap.set({'n', 'v', 'o'}, "z#", "<Plug>(asterisk-z#)<Plug>(is-nohl-10)",
     { silent = true, remap = true, desc = "Search backward (in-place)" })
 vim.keymap.set({'n', 'v', 'o'}, "gz#", "<Plug>(asterisk-gz#)<Plug>(is-nohl-10)",
