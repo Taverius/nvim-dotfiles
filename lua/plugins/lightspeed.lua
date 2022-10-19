dd5ff7918f1f4cb0f553161e8d899d1462b952b3
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-19 12:12:55 +0200

    Update lightspeed.lua
    * Explicitly bind in legendary as which-key only partially works on stock bindings

--- lua/plugins/lightspeed.lua
+++ lua/plugins/lightspeed.lua
@@ -5,20 +5,50 @@ require('lightspeed').setup {
     match_only_the_start_of_same_char_seqs = true,
 }
 
--- Which-keu
+-- Which-key/Legendary registrations
+-- Norma-mode maps
 require("which-key").register({
-    s = { name = "Search forward", mode = 'nv' },
-    S = { name = "Search backward", mode = 'nv' },
-    z = { name = "Select until forward", mode = 'o' },
-    Z = { name = "Select until backward", mode = 'o' },
-    x = { name = "Select including forward", mode = 'o' },
-    X = { name = "Search including backward", mode = 'o' },
-    gs = { name = "Search in next windows", mode = 'n' },
-    gS = { name = "Search in previous windows", mode = 'n' },
-    f = { name = "Search forward including", mode = 'nvo' },
-    F = { name = "Search backward including", mode = 'nvo' },
-    t = { name = "Search forward until", mode = 'nvo' },
-    T = { name = "Search backward until", mode = 'nvo' },
-    })
-
-
+    ["s"] = "Search forward",
+    ["S"] = "Search backward",
+    ["gs"] = "Search in next windows",
+    ["gS"] = "Search in previous windows",
+    ["f"] = "Search forward including",
+    ["F"] = "Search backward including",
+    ["t"] = "Search forward until",
+    ["T"] = "Search backward until",
+    }, { mode = 'n' })
+-- Visual-mode maps
+require("which-key").register({
+    ["s"] = "Search forward",
+    ["S"] = "Search backward",
+    ["f"] = "Search forward including",
+    ["F"] = "Search backward including",
+    ["t"] = "Search forward until",
+    ["T"] = "Search backward until",
+    }, { mode = 'v' })
+-- Operator-mode maps
+require("which-key").register({
+    ["z"] = "Select until forward",
+    ["Z"] = "Select until backward",
+    ["x"] = "Select including forward",
+    ["X"] = "Search including backward",
+    ["f"] = "Search forward including",
+    ["F"] = "Search backward including",
+    ["t"] = "Search forward until",
+    ["T"] = "Search backward until",
+    }, { mode = 'o' })
+-- Legendary
+require("legendary").bind_keymaps({
+    { 's', description = "Search forward", mode = { 'n', 'v' } },
+    { 'S', description = "Search backward", mode = { 'n', 'v' } },
+    { 'z', description = "Select until forward", mode = { 'o' } },
+    { 'Z', description = "Select until backward", mode = { 'o' } },
+    { 'x', description = "Select including forward", mode = { 'o' } },
+    { 'X', description = "Search including backward", mode = { 'o' } },
+    { 'gs', description = "Search in next windows", mode = { 'n' } },
+    { 'gS', description = "Search in previous windows", mode = { 'n' } },
+    { 'f', description = "Search forward including", mode = { 'n', 'v', 'o' } },
+    { 'F', description = "Search backward including", mode = { 'n', 'v', 'o' } },
+    { 't', description = "Search forward until", mode = { 'n', 'v', 'o' } },
+    { 'T', description = "Search backward until", mode = { 'n', 'v', 'o' } },
+})
