68191e75000eae57829e579f80d3abe0ac65ee55
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2022-10-19 10:36:14 +0200

    Update is.lua
    * Map descriptions for which-key

--- lua/plugins/is.lua
+++ lua/plugins/is.lua
@@ -2,13 +2,21 @@
 -- lua/plugins/is.lua @ Leonardo Valeri Manera 2022
 
 -- vim-asterisk maps
-vim.keymap.set({'n', 'v', 'o'}, "*", "<Plug>(asterisk-*)<Plug>(is-nohl-1)", { silent = true, remap = true })
-vim.keymap.set({'n', 'v', 'o'}, "g*", "<Plug>(asterisk-g*)<Plug>(is-nohl-1)", { silent = true, remap = true })
-vim.keymap.set({'n', 'v', 'o'}, "#", "<Plug>(asterisk-#)<Plug>(is-nohl-1)", { silent = true, remap = true })
-vim.keymap.set({'n', 'v', 'o'}, "g#", "<Plug>(asterisk-g#)<Plug>(is-nohl-1)", { silent = true, remap = true })
+vim.keymap.set({'n', 'v', 'o'}, "*", "<Plug>(asterisk-*)<Plug>(is-nohl-1)",
+    { silent = true, remap = true, desc = "Search forward" })
+vim.keymap.set({'n', 'v', 'o'}, "g*", "<Plug>(asterisk-g*)<Plug>(is-nohl-1)",
+    { silent = true, remap = true, desc = "Partial Search forward" })
+vim.keymap.set({'n', 'v', 'o'}, "#", "<Plug>(asterisk-#)<Plug>(is-nohl-1)",
+    { silent = true, remap = true, desc = "Search backward" })
+vim.keymap.set({'n', 'v', 'o'}, "g#", "<Plug>(asterisk-g#)<Plug>(is-nohl-1)",
+    { silent = true, remap = true, desc = "Partial search backward" })
 
-vim.keymap.set({'n', 'v', 'o'}, "z*", "<Plug>(asterisk-z*)<Plug>(is-nohl-10)", { silent = true, remap = true })
-vim.keymap.set({'n', 'v', 'o'}, "gz*", "<Plug>(asterisk-gz*)<Plug>(is-nohl-10)", { silent = true, remap = true })
-vim.keymap.set({'n', 'v', 'o'}, "z#", "<Plug>(asterisk-z#)<Plug>(is-nohl-10)", { silent = true, remap = true })
-vim.keymap.set({'n', 'v', 'o'}, "gz#", "<Plug>(asterisk-gz#)<Plug>(is-nohl-10)", { silent = true, remap = true })
+vim.keymap.set({'n', 'v', 'o'}, "z*", "<Plug>(asterisk-z*)<Plug>(is-nohl-10)",
+    { silent = true, remap = true, desc = "Search forward (in-place)" })
+vim.keymap.set({'n', 'v', 'o'}, "gz*", "<Plug>(asterisk-gz*)<Plug>(is-nohl-10)",
+    { silent = true, remap = true desc = "Partial Search forward (in-place)" })
+vim.keymap.set({'n', 'v', 'o'}, "z#", "<Plug>(asterisk-z#)<Plug>(is-nohl-10)",
+    { silent = true, remap = true, desc = "Search backward (in-place)" })
+vim.keymap.set({'n', 'v', 'o'}, "gz#", "<Plug>(asterisk-gz#)<Plug>(is-nohl-10)",
+    { silent = true, remap = true, desc = "Partial search backward (in-place)" })
 
