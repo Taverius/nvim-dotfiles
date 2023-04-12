4b272d0de1635107e423249b72ff1858c8d48ca9
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2023-04-12 17:03:01 +0200

    Update colorscheme.lua
    * github theme lua style selector has been deprecated

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,8 +1,6 @@
 --   vi: tw=100
 -- lua/colorscheme.lua @ Leonardo Valeri Manera 2022
 
--- github_dimmed
-require("github-theme").setup({ theme_style = "dimmed", })
 -- gruvbox
 require("gruvbox").setup({ contrast = "soft", })
 -- melange
