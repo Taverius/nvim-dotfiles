0b4b5ac59b6c15a3c9d547ddecae3ea560714ba1
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2023-07-07 21:38:04 +0200

    Update lualine.lua
    * Add Lazy.nvim update checker component

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,16 @@
 --   vi: tw=100
 -- lua/plugins/lualine.lua @ Leonardo Valeri Manera 2022
 
-require('lualine').setup { extensions = { 'fugitive', 'fern', 'fzf', 'quickfix' } }
+require("lualine").setup {
+    extensions = { "fugitive", "fern", "fzf", "quickfix" },
+    sections = {
+        lualine_x = {
+            {
+                require("lazy.status").updates,
+                cond = require("lazy.status").has_updates,
+                color = { fg = "#ff9e64" },
+            },
+        },
+    },
+}
 
