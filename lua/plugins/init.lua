8bfd178f96b5294cc240e160fa6764fe7ab82e43
Leonardo Valeri Manera <l.valerimanera@gmail.com>
2024-12-10 08:15:31 +0100

    Update init.lua
    * Update lazy.nvim bootstrap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,16 +4,20 @@
 local opt = vim.opt
 local fn = vim.fn
 
+-- Bootstrap lazy.nvim
 local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    fn.getchar()
+    os.exit(1)
+  end
 end
 opt.rtp:prepend(lazypath)
 
